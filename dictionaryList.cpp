f2965aaf4c53baf804cf84d4ffddd13adb89ed17
DarinVicaldo <113657561+DarinVicaldo@users.noreply.github.com>
2024-09-10 14:08:57 -0600

    Exercise B files

---
+++ dictionaryList.cpp
@@ -0,0 +1,252 @@
+// lookuptable.cpp
+
+// Lab 1 - Exercise A
+ 
+// Completed by:
+
+#include <assert.h>
+#include <iostream>
+#include <stdlib.h>
+#include "dictionaryList.h"
+#include "mystring_B.h"
+
+using namespace std;
+
+Node::Node(const Key& keyA, const Datum& datumA, Node *nextA)
+  : keyM(keyA), datumM(datumA), nextM(nextA)
+{
+}
+
+DictionaryList::DictionaryList()
+  : sizeM(0), headM(0), cursorM(0)
+{
+}
+
+DictionaryList::DictionaryList(const DictionaryList& source)
+{
+  copy(source);
+}
+
+DictionaryList& DictionaryList::operator =(const DictionaryList& rhs)
+{
+  if (this != &rhs) {
+    destroy();
+    copy(rhs);
+  }
+  return *this;
+}
+
+DictionaryList::~DictionaryList()
+{
+  destroy();
+}
+
+int DictionaryList::size() const
+{
+  return sizeM;
+}
+
+int DictionaryList::cursor_ok() const
+{
+  return cursorM != 0;
+}
+
+const Key& DictionaryList::cursor_key() const
+{
+  assert(cursor_ok());
+  return cursorM->keyM;
+}
+
+const Datum& DictionaryList::cursor_datum() const
+{
+  assert(cursor_ok());
+  return cursorM->datumM;
+}
+
+void DictionaryList::insert(const int& keyA, const Mystring& datumA)
+{
+  // Add new node at head?                                                                                  
+  if (headM == 0 || keyA < headM->keyM) {
+    headM = new Node(keyA, datumA, headM);
+    sizeM++;
+  }
+	
+  // Overwrite datum at head?                                                                               
+  else if (keyA == headM->keyM)
+    headM->datumM = datumA;
+	
+  // Have to search ...                                                                                     
+  else {
+      
+    //POINT ONE
+		
+    // if key is found in list, just overwrite data;                                                        
+    for (Node *p = headM; p !=0; p = p->nextM)
+		{
+			if(keyA == p->keyM)
+			{
+				p->datumM = datumA;
+				return;
+			}
+		}
+		
+    //OK, find place to insert new node ...                                                                 
+    Node *p = headM ->nextM;
+    Node *prev = headM;
+		
+    while(p !=0 && keyA >p->keyM)
+		{
+			prev = p;
+			p = p->nextM;
+		}
+		
+    prev->nextM = new Node(keyA, datumA, p);
+    sizeM++;
+  }
+  cursorM = NULL;
+	
+}
+
+void DictionaryList::remove(const int& keyA)
+{
+    if (headM == 0 || keyA < headM -> keyM)
+        return;
+    
+    Node *doomed_node = 0;
+    
+    if (keyA == headM-> keyM) {
+        doomed_node = headM;
+        headM = headM->nextM;
+        
+        // POINT TWO
+    }
+    else {
+        Node *before = headM;
+        Node *maybe_doomed = headM->nextM;
+        while(maybe_doomed != 0 && keyA > maybe_doomed-> keyM) {
+            before = maybe_doomed;
+            maybe_doomed = maybe_doomed->nextM;
+        }
+        
+        if (maybe_doomed != 0 && maybe_doomed->keyM == keyA) {
+            doomed_node = maybe_doomed;
+            before->nextM = maybe_doomed->nextM;
+        }
+        
+        
+    } 
+    if(doomed_node == cursorM)
+        cursorM = 0;
+    
+    delete doomed_node;           // Does nothing if doomed_node == 0.
+    sizeM--;
+}
+
+void DictionaryList::go_to_first()
+{
+    cursorM = headM;
+}
+
+void DictionaryList::step_fwd()
+{
+    assert(cursor_ok());
+    cursorM = cursorM->nextM;
+}
+
+void DictionaryList::make_empty()
+{
+    destroy();
+    sizeM = 0;
+    cursorM = 0;
+}
+
+
+// The following function are supposed to be completed by the stuents, as part
+// of the exercise B part II. the given fucntion are in fact place-holders for
+// find, destroy and copy, in order to allow successful linking when you're
+// testing insert and remove. Replace them with the definitions that work.
+
+// The following function are supposed to be completed by the stuents, as part
+// of the exercise B part II. the given fucntion are in fact place-holders for
+// find, destroy and copy, in order to allow successful linking when you're
+// testing insert and remove. Replace them with the definitions that work.
+
+
+void DictionaryList::find(const Key& keyA)
+{
+  // Traverse the list to find the node with the correct key.
+  Node* temp = headM;
+
+
+  while(temp != 0 && temp->keyM != keyA){
+    temp = temp->nextM;
+  }
+
+
+  // If the node is found, set cursorM to the node. If not, it will print that the key does not exist.
+  if(temp != 0){
+    cursorM = temp;
+    return;
+  } else {
+    cursorM = 0;
+    cout << "The key does not exist.";
+    return;
+  }
+
+
+}
+
+void DictionaryList::destroy()
+{
+  Node* temp = headM;
+
+
+  // While nodes still exist, Keep traversing and deleting.
+  while (temp != 0){
+    Node* next = temp->nextM;
+    delete temp;
+    temp = next;
+  }  
+
+
+  headM = 0;
+  sizeM = 0;
+  cursorM = 0;
+  return;
+}
+
+void DictionaryList::copy(const DictionaryList& source)
+{
+  destroy();
+
+  // If source is empty, nothing to copy
+  if (source.headM == nullptr) {
+    headM = nullptr;
+    sizeM = 0;
+    cursorM = nullptr;
+    return;
+  }
+
+  Node* sourceptr = source.headM;
+  Node* last = nullptr;
+
+  // Copy the nodes from source to this list
+  while (sourceptr != nullptr) {
+    Node* newNode = new Node(sourceptr->keyM, sourceptr->datumM, nullptr);
+    
+    if (headM == nullptr) {
+      headM = newNode;
+    } else {
+      last->nextM = newNode;
+    }
+
+    last = newNode;
+    sourceptr = sourceptr->nextM;
+  }
+
+  sizeM = source.sizeM;
+  return;
+}
+
+
+
